393ef6fbe98e6e675e79fcb5d579dced36e81880
Taras Dashkevych <trdashix@gmail.com>
2017-08-25 23:38:35 +0300

    Add: toggling visibility of the control(s) based on the "Blog View: Featured Page" option.

--- inc/customizer/js/customize-controls.js
+++ inc/customizer/js/customize-controls.js
@@ -1,3 +1,6 @@
+/**
+ * Font family control.
+ */
 wp.customize.controlConstructor['font-family'] = wp.customize.Control.extend({
 	ready: function () {
 		var control = this;
@@ -20,3 +23,45 @@ wp.customize.controlConstructor['font-family'] = wp.customize.Control.extend({
 		);
 	}
 });
+
+/**
+ * Checks that all controls and settings are loaded.
+ */
+wp.customize.bind( 'ready', function() {
+
+	// Customize object alias.
+    var customize = this;
+
+	// Toggling visibility of the control(s) based on the "Blog View: Featured Page" option.
+	customize( 'goedemorgen_settings[archive][featured_page_id]', function( setting ) {
+		var setupControl = function( control ) {
+			var setActiveState, isDisplayed;
+
+			// Determinate if we want to show or hide the control.
+			isDisplayed = function() {
+				if (
+					( 0 === setting.get() || '0' === setting.get() ) &&
+					'page' !== customize.control( 'show_on_front' ).setting.get()
+				) {
+					return false;
+				} else {
+					return true;
+				}
+			};
+
+			// Activate or deactivate a control.
+			setActiveState = function() {
+				control.active.set( isDisplayed() );
+			};
+
+			// Initial state of the control.
+			setActiveState();
+
+			// Toggling visibility of the control.
+			control.active.validate = isDisplayed;
+			setting.bind( setActiveState );
+		};
+
+		customize.control( 'goedemorgen_is_posts_page_header', setupControl );
+	} );
+} );
