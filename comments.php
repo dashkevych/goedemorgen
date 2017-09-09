10f4b1c5d4a434fdd1e0380dce2765253e134818
Taras Dashkevych <trdashix@gmail.com>
2017-09-08 19:30:27 -0700

    Remove: superfluous check from comment template;

--- comments.php
+++ comments.php
@@ -74,10 +74,8 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-
 	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-
+	if ( ! comments_open() ) : ?>
 		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'goedemorgen' ); ?></p>
 	<?php
 	endif;
