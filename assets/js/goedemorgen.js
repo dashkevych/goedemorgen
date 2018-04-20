0c98aba8d009ba9c8fb2300ad58a9ae20bd35cb1
Taras Dashkevych <trdashix@gmail.com>
2018-04-19 18:40:00 -0700

    Fix: optimize the theme JS file and improve performance by avoiding to call the ready handler multiple times;

--- assets/js/goedemorgen.js
+++ assets/js/goedemorgen.js
@@ -1,70 +1,78 @@
-jQuery( document ).ready( function() {
+"use strict";
+
+/*
+ Handles additional functionalities of the theme.
+*/
+
+(function(){
+
 	var documentBody = jQuery( document.body );
 	var browserWindow = jQuery( window );
 	var pageContainer = jQuery( document.getElementById( 'page' ) );
 	var headerContainer = pageContainer.find( document.getElementById( 'masthead' ) );
 	var contentContainer = pageContainer.find( document.getElementById( 'content' ) );
 
-	// Add custom class to table element and make it responsive.
-	jQuery( function() {
-		jQuery( 'table' ).addClass( 'table' ).wrap( '<div class="table-responsive" />' );
+	// Things that need to happen when the document is ready.
+	jQuery(function() {
+		// Add custom class to table element and make it responsive.
+		(function(){
+			jQuery( 'table' ).addClass( 'table' ).wrap( '<div class="table-responsive" />' );
 
-		var infiniteCount = 0;
+			var infiniteCount = 0;
 
-		documentBody.on( 'post-load', function() {
+			documentBody.on( 'post-load', function() {
 
-			infiniteCount = infiniteCount + 1;
+				infiniteCount = infiniteCount + 1;
 
-			var infiniteItems = jQuery( '.infinite-wrap.infinite-view-' + infiniteCount );
-			infiniteItems.find( 'table' ).addClass( 'table' ).wrap( '<div class="table-responsive" />' );
-		});
-	});
+				var infiniteItems = jQuery( '.infinite-wrap.infinite-view-' + infiniteCount );
+				infiniteItems.find( 'table' ).addClass( 'table' ).wrap( '<div class="table-responsive" />' );
+			});
+		})();
 
-	// Header search form.
-	jQuery( function() {
+		// Header search form.
 		headerContainer.on( 'click', '#header-search-button', function(e) {
 			e.preventDefault();
 			jQuery( this ).closest( '.main-navigation' ).toggleClass( 'active-search-form' );
 		});
-	});
 
-	// Mobile Menu.
-	jQuery( function() {
-		var toggleMenu = pageContainer.find( document.getElementById( 'toggle-menu' ) );
-		headerContainer.find( '.menu:not(.social-menu)' ).clone().appendTo( '#mobile-navigation' );
+		// Mobile Menu.
+		(function(){
+			var toggleMenu = pageContainer.find( document.getElementById( 'toggle-menu' ) );
+			headerContainer.find( '.menu:not(.social-menu)' ).clone().appendTo( '#mobile-navigation' );
 
-		documentBody.on( 'click', '#mobile-menu-toggle, #close-toggle-menu', function(e) {
-			documentBody.toggleClass( 'toggle-mobile-menu' );
-			toggleMenu.slideToggle( 'fast' );
-		});
-	});
-
-	// Back to Top button.
-	jQuery( function() {
-		var backToTopButton = jQuery( document.getElementById( 'backtotop-button' ) );
-		var buttonSettings = { opacity: '0', visibility: 'hidden' };
-
-		browserWindow.scroll(function() {
-			if ( browserWindow.scrollTop() > 300 ) {
-				buttonSettings.opacity = 1;
-				buttonSettings.visibility = 'visible';
-			} else {
-				buttonSettings.opacity = 0;
-				buttonSettings.visibility = 'hidden';
-			}
-
-			backToTopButton.css({
-				'opacity': buttonSettings.opacity,
-				'visibility': buttonSettings.visibility
+			documentBody.on( 'click', '#mobile-menu-toggle, #close-toggle-menu', function(e) {
+				documentBody.toggleClass( 'toggle-mobile-menu' );
+				toggleMenu.slideToggle( 'fast' );
 			});
-		});
+		})();
 
-		backToTopButton.on( 'click', function( e ) {
-			e.preventDefault();
+		// Back to Top button.
+		(function(){
+			var backToTopButton = jQuery( document.getElementById( 'backtotop-button' ) );
+			var buttonSettings = { opacity: '0', visibility: 'hidden' };
 
-			jQuery( 'html, body' ).animate({
-				scrollTop: 0
-			}, 200);
-		});
+			browserWindow.scroll(function() {
+				if ( browserWindow.scrollTop() > 300 ) {
+					buttonSettings.opacity = 1;
+					buttonSettings.visibility = 'visible';
+				} else {
+					buttonSettings.opacity = 0;
+					buttonSettings.visibility = 'hidden';
+				}
+
+				backToTopButton.css({
+					'opacity': buttonSettings.opacity,
+					'visibility': buttonSettings.visibility
+				});
+			});
+
+			backToTopButton.on( 'click', function( e ) {
+				e.preventDefault();
+
+				jQuery( 'html, body' ).animate({
+					scrollTop: 0
+				}, 200);
+			});
+		})();
 	});
-});
+})();
