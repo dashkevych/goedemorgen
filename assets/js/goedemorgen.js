c3e280ba4d93b8bf6c7ef38ed4d1858913d2656d
Taras Dashkevych <trdashix@gmail.com>
2018-04-27 19:16:02 -0700

    Update: reorganize the theme JS file;

--- assets/js/goedemorgen.js
+++ assets/js/goedemorgen.js
@@ -6,50 +6,77 @@
 
 (function(){
 
-	var documentBody = jQuery( document.body );
-	var browserWindow = jQuery( window );
-	var pageContainer = jQuery( document.getElementById( 'page' ) );
-	var headerContainer = pageContainer.find( document.getElementById( 'masthead' ) );
-	var contentContainer = pageContainer.find( document.getElementById( 'content' ) );
+	var goedemorgenTheme = {
+		pageContainer: jQuery( document.getElementById( 'page' ) ),
+		headerContainer: jQuery( document.getElementById( 'page' ) ).find( document.getElementById( 'masthead' ) ),
+		contentContainer: jQuery( document.getElementById( 'page' ) ).find( document.getElementById( 'content' ) ),
 
-	// Things that need to happen when the document is ready.
-	jQuery(function() {
+		getDocumentBody: function() {
+			return jQuery( document.body );
+		},
+
+		getBrowserWindow: function() {
+			return jQuery( window );
+		},
+
+		// Run on ready.
+		onReady: function() {
+			this.createResponsiveTables();
+
+		},
+
+		// Run on load.
+		onLoad: function() {
+			this.displayMobileMenu();
+			this.displayHeaderSearchForm();
+			this.addBackToTopButton();
+		},
+
 		// Add custom class to table element and make it responsive.
-		(function(){
+		createResponsiveTables: function() {
 			jQuery( 'table' ).addClass( 'table' ).wrap( '<div class="table-responsive" />' );
 
-			var infiniteCount = 0;
+			var infiniteCount, infiniteItems, documentBody;
+
+			infiniteCount = 0;
+			documentBody = this.getDocumentBody();
 
 			documentBody.on( 'post-load', function() {
 
 				infiniteCount = infiniteCount + 1;
 
-				var infiniteItems = jQuery( '.infinite-wrap.infinite-view-' + infiniteCount );
+				infiniteItems = jQuery( '.infinite-wrap.infinite-view-' + infiniteCount );
 				infiniteItems.find( 'table' ).addClass( 'table' ).wrap( '<div class="table-responsive" />' );
 			});
-		})();
+		},
 
-		// Header search form.
-		headerContainer.on( 'click', '#header-search-button', function(e) {
-			e.preventDefault();
-			jQuery( this ).closest( '.main-navigation' ).toggleClass( 'active-search-form' );
-		});
+		// Create a header search form.
+		displayHeaderSearchForm: function() {
+			this.headerContainer.on( 'click', '#header-search-button', function(e) {
+				e.preventDefault();
+				jQuery( this ).closest( '.main-navigation' ).toggleClass( 'active-search-form' );
+			});
+		},
 
-		// Mobile Menu.
-		(function(){
-			var toggleMenu = pageContainer.find( document.getElementById( 'toggle-menu' ) );
-			headerContainer.find( '.menu:not(.social-menu)' ).clone().appendTo( '#mobile-navigation' );
+		// Create a mobile menu.
+		displayMobileMenu: function() {
+			var toggleMenu = this.pageContainer.find( document.getElementById( 'toggle-menu' ) );
+			var documentBody = this.getDocumentBody();
+			this.headerContainer.find( '.menu:not(.social-menu)' ).clone().appendTo( '#mobile-navigation' );
 
 			documentBody.on( 'click', '#mobile-menu-toggle, #close-toggle-menu', function(e) {
 				documentBody.toggleClass( 'toggle-mobile-menu' );
 				toggleMenu.slideToggle( 'fast' );
 			});
-		})();
+		},
 
-		// Back to Top button.
-		(function(){
-			var backToTopButton = jQuery( document.getElementById( 'backtotop-button' ) );
-			var buttonSettings = { opacity: '0', visibility: 'hidden' };
+		// Add Back to Top button functionality.
+		addBackToTopButton: function() {
+			var backToTopButton, buttonSettings, browserWindow;
+
+			backToTopButton = jQuery( document.getElementById( 'backtotop-button' ) );
+			buttonSettings = { opacity: '0', visibility: 'hidden' };
+			browserWindow = this.getBrowserWindow();
 
 			browserWindow.scroll(function() {
 				if ( browserWindow.scrollTop() > 300 ) {
@@ -73,6 +100,16 @@
 					scrollTop: 0
 				}, 200);
 			});
-		})();
+		}
+	};
+
+	// Things that need to happen when the document is ready.
+	jQuery(function() {
+		goedemorgenTheme.onReady();
+	});
+
+	// Things that need to happen after a full load.
+	jQuery( window ).on( 'load', function() {
+		goedemorgenTheme.onLoad();
 	});
 })();
